bbac87efe0cafc0f3644d3474fcff706c4fcd1d7
Shahzeb Ali <88718611+ishahzebali@users.noreply.github.com>
2024-07-04 15:55:52 +0500

    updated some things

--- lib/expenses.dart
+++ lib/expenses.dart
@@ -1,3 +1,5 @@
+import 'package:expense_tracker/expense_list.dart';
+import 'package:expense_tracker/models/expense.dart';
 import 'package:flutter/material.dart';
 
 class Expenses extends StatefulWidget {
@@ -9,13 +11,28 @@ class Expenses extends StatefulWidget {
 }
 
 class _ExpensesState extends State<Expenses> {
+  final List<Expense> _regesteredExpenses = [
+    Expense(
+        amount: 19.99,
+        date: DateTime.now(),
+        title: 'Flutter Course',
+        category: Category.work),
+    Expense(
+        amount: 15.69,
+        date: DateTime.now(),
+        title: 'Cinema',
+        category: Category.leisure),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Column(children: [
-        Text('Espenses Chart'),
-        Text('Expenses List'),
-      ],),
+    return Scaffold(
+      body: Column(
+        children: [
+          const Text('Espenses Chart'),
+          Expanded(child: ExpenseList(expenses: _regesteredExpenses))
+        ],
+      ),
     );
   }
 }
